dcd571c63db1f217707c4455c7bbfaf23d295a4a
srinivas <srinivash1602@gmail.com>
2026-06-26 16:45:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
+// =============================
+// Scroll To Top
+// =============================
 
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>300){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+
+});
+
+topBtn.onclick=()=>{
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+};
+
+// =============================
+// Fade Animation
+// =============================
+
+const sections=document.querySelectorAll("section");
+
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+sections.forEach(section=>{
+
+section.classList.add("fade");
+
+observer.observe(section);
+
+});
